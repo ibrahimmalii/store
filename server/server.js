db8cc7f02c45df163bb4151940f73fdb99e81086
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-10-01 16:19:39 +0200

    add route for get transactions

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('store', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+
+
+app.get('/transactions/:query?', async(req, res)=>{
+    const {id, startDate, endDate} = req.query;
+    if(id){
+        const transaction = await sequelize.query(`select * from transactions WHERE seller_id = ${id}`);
+        res.send(transaction[0]);
+    }else if(id && startDate && endDate){
+        const transaction = await sequelize.query(`select * from transactions WHERE (seller_id = ${id} AND last_updated BETWEEN ${startDate} AND ${endDate} )`);
+        res.send(transaction[0]);
+    }else{
+        res.send('data invalid');
+    }
+})
+
+
+app.listen(port, ()=>console.log('Now your server up to:',port));
+
